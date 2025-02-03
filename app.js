271d51e938695f206629a74770e615f5b45c807b
Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2025-02-03 11:12:31 -0300

    Adiciona o código da lógica

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-let numeroSecreto = 5;
-alert(numeroSecreto);
+alert("Bem vindo ao jogo da advinhação!");
+let maximo = prompt(`Escolha o número máximo.`);
+let numeroSecreto = parseInt(Math.random() * maximo + 1);
+console.log(numeroSecreto);
+let chute = prompt(`Agora chute um número entre 0 e ${maximo}.`);
+let cont = 1;
+
+while (numeroSecreto != chute) {
+    cont ++;
+    if (chute < numeroSecreto) {
+        chute = prompt(`Errou!! O número secreto é maior que ${chute}.`);
+    } else {
+        chute = prompt(`Errou!! O número secreto é menor que ${chute}.`);
+    }
+}
+let tentativa = cont == 1 ? "tentativa" : "tentativas";
+alert(`Parabéns!! Você acertou em ${cont} ${tentativa}.`);
